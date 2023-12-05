8478853c85fc0b265ee17f37daa1bcc74906cdc8
Yash Bhardwaj <yashpremsharma@gmail.com>
2023-12-05 21:21:43 +0530

    remove unused code

--- app/src/main/java/com/epsoftandroid/demo/utils/Extension.kt
+++ app/src/main/java/com/epsoftandroid/demo/utils/Extension.kt
@@ -9,34 +9,8 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 
-fun isInternetAvailable(ctx: Context): Boolean {
-    var result = false
-    val connectivityManager = ctx.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-
-    connectivityManager?.let {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
-                result = when {
-                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                    else -> false
-                }
-            }
-        } else {
-            connectivityManager.activeNetworkInfo.also { networkInfo ->
-                return networkInfo != null && networkInfo.isConnected
-            }
-        }
-    }
-    return result
-}
-
 fun showToast(context: Context?, message: String?) {
     Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-}
-
-fun nullSafeErrorLogging(errorMessage: String?): Exception {
-    return if (errorMessage != null) EWException(errorMessage) else EWException(Constant.NULL_ERROR_MESSAGE)
 }
 
 fun setButtonActivatedWithColor(
